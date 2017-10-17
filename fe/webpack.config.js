eb99d6cddd54fca50bd77d6b085c1c72a9b39ad5
minu youn <anomie7777@gmail.com>
2017-10-17 21:53:14 +0900

    added file-loader

--- fe/webpack.config.js
+++ fe/webpack.config.js
@@ -43,6 +43,18 @@ module.exports = {
           'style-loader',
           'css-loader'
         ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
       }
     ]
   },
